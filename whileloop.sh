e5f3f77624800e0fe17859c16ebd8fcc1d20ad09
Ridham <ridhampatel2002@gmail.com>
2024-01-31 15:04:03 +0530

    new commit

---
+++ whileloop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#while loop
+
+idx=1
+
+while [ idx -lt 5 ]
+do 
+    echo "The current number is $idx"
+    let "idx++"     #let command used for arithmetic operations. It allows you to
+done
