56d30df59f31bd40e52e7c50bb3ec8fc97b14eb8
Ridham <ridhampatel2002@gmail.com>
2024-02-01 10:29:02 +0530

    Complete for loop

--- forloop.sh
+++ forloop.sh
@@ -5,3 +5,27 @@ do
     echo $VARIABLE
 done
 
+for VARIABLE in {1..10..2} #{START..END..INCREMENT}
+do
+    echo $VARIABLE
+done
+
+for (( i=0; i<=5 ; i++ ))
+do
+    echo $i
+done
+
+
+for command in ls pwd date 
+do
+    echo "------------------$command--------------------"
+    $command
+done
+
+for item in ~/Desktop/*
+do
+    if [ -d $item ]
+    then 
+        echo "$item is a directory."
+    fi
+done
